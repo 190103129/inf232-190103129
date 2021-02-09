f85d585134c2296c4883f044c382f5a8f692b91d
Sagadat Yesbolsynov <190103129@stu.sdu.edu.kz>
2021-02-09 14:02:12 +0600

    added FinalOne

---
+++ FinalOne.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class FinalOne{
+	public static void main(String[] args) {
+		 for(int i=1;i<7;i++){
+		 	System.out.println("");
+		 	for(int j=1;j<=i;j++){
+		 		System.out.print(j+" ");
+		 	}
+		 }
+	}
+}
